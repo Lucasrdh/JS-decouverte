742123e03f1b0ed13242834363f607fc3a80756b
Lucas CHRETIEN <lucas.chretien@Digital-Grenoble.local>
2024-09-02 14:40:22 +0200

    ex0 4

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,12 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+console.log(secretMessage);
+let last =secretMessage.pop();
+let newLength = secretMessage.push("to","program");
+secretMessage[6] = "right";
+let first = secretMessage.shift();
+let newLenght = secretMessage.unshift("Programming");
+secretMessage.splice(6, 5, "know");
+
+console.log(secretMessage.join());
